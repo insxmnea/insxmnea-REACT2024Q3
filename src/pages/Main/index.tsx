1fb3801821261fd7025c64c4a32d9489e2257a52
1317n0d3 <kostya.svetashov.01@gmail.com>
2024-07-07 20:26:58 +0400

    feat: add loader

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -5,18 +5,21 @@ import { Deal } from "../../services/models";
 import DealCard from "../../components/DealCard";
 import SearchBar from "../../components/SearchBar";
 import historyService from "../../services/historyService";
+import Loader from "../../components/Loader";
 
 type Props = {};
 
 type State = {
   deals: Deal[];
   hasError: boolean;
+  noResults: boolean;
 };
 
 class Main extends Component<Props, State> {
   state: State = {
     deals: [],
     hasError: false,
+    noResults: false,
   };
 
   componentDidMount() {
@@ -24,8 +27,9 @@ class Main extends Component<Props, State> {
   }
 
   onSearch = async (search: string = "") => {
+    this.setState({ deals: [], noResults: false });
     const res = await getDeals(search);
-    this.setState({ deals: res });
+    this.setState({ deals: res, noResults: res.length === 0 });
   };
 
   render() {
@@ -46,6 +50,12 @@ class Main extends Component<Props, State> {
             Throw error
           </button>
         </header>
+
+        {this.state.deals.length === 0 && !this.state.noResults && <Loader />}
+
+        {this.state.noResults && (
+          <div className={styles.noResults}>Not found</div>
+        )}
 
         <div className={styles.deals}>
           {this.state.deals.map((deal) => (
