8e77187d03d1a39192ab0aa4f122ca576f4496ca
1317n0d3 <kostya.svetashov.01@gmail.com>
2024-07-07 21:33:01 +0400

    feat: update project config for deploy to gh pages

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,11 +3,7 @@ import { Deal } from "./models";
 const URL = "https://www.cheapshark.com/api/1.0";
 
 const api = {
-  getDeals: async (
-    title: string = "",
-    pageNumber: number = 1,
-    pageSize: number = 50
-  ): Promise<Deal[]> => {
+  getDeals: async (title: string = ""): Promise<Deal[]> => {
     const query = `${URL}/deals?storeID=1&title=${title}`;
     return await fetch(query)
       .then((response) => response.json())
