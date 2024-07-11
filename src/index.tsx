9761e7d9101d41e7bd7a1534608b81e66062677a
1317n0d3 <kostya.svetashov.01@gmail.com>
2024-07-11 19:29:37 +0400

    feat: add and configure react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,29 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.scss";
 import Main from "./pages/Main/index.tsx";
-import ErrorBoundary from "./components/ErrorBoundary/index.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./pages/NotFound/index.tsx";
+import ErrorBoundaryLayout from "./components/ErrorBoundaryLayout/index.tsx";
+
+const router = createBrowserRouter([
+  {
+    element: <ErrorBoundaryLayout />,
+    children: [
+      {
+        path: "/insxmnea-REACT2024Q3/",
+        element: <Main />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <Main />
-    </ErrorBoundary>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
